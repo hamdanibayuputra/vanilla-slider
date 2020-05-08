26a37aaf6157bc9280bcd66ff29050c318b70e62
hamdanibayup <hamdanigithub@gmail.com>
2020-05-09 00:46:29 +0700

    first commit eaaa..

---
+++ script.js
@@ -0,0 +1,30 @@
+const container = document.querySelector(".container");
+const image = document.querySelector(".image");
+const circles = document.querySelectorAll(".circle");
+
+container.addEventListener("click", circleSlide);
+
+function circleSlide(e) {
+  image.animate(
+    [{ opacity: "0.1" }, { opacity: "1" }], 
+    { duration: 500, fill: "forwards" }
+  )
+
+  circles.forEach(function(circle, index) {
+    if(e.target.classList.contains(`c-${index+1}`)) {
+      image.src = `images/${index+1}.jpg`;
+    }
+  })
+
+  if(e.target.classList.contains("circle")) {
+    circles.forEach(function(circle) {
+      // check class "active"
+      if(circle.classList.contains("active")) {
+        circle.classList.remove("active");
+      }
+    })
+
+    // add class "active"
+    e.target.classList.add("active");
+  }
+}
